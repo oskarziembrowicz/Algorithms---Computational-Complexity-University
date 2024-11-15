bbe4e88f946e23a4bc0b68af3ae8add17245a668
oskarziembrowicz <rakson2002@gmail.com>
2024-11-15 15:58:10 +0100

    Started writing quick sort

--- lab5/quick-sort-test.cpp
+++ lab5/quick-sort-test.cpp
@@ -3,49 +3,65 @@
 
 using namespace std;
 
-vector<int> quickSort(vector<int> arr, int start, int end, int layer) {
-    if (start < end) {
-        // Set pivot point
-        int pivot = arr[end];
-        // cout << "Pivot: "  << pivot << "\n";
-        // MAYBE DONT SUBTRACT 1?
-        int before_index = (start-1);
+int partition(vector<int>& arr, int low, int high) {
+  
+    // Choose the pivot
+    int pivot = arr[high];
+  
+    // Index of smaller element and indicates 
+    // the right position of pivot found so far
+    int i = low - 1;
 
-        // Swap elements based on pivot
-        for (int i=start; i<end; i++) {
-            if (arr[i] <= pivot) {
-                // PUSH THIS ATHER THE SWAP?
-                before_index++;
-                swap(arr[before_index], arr[i]);
-                cout << "Swapped " << arr[before_index] << " and " << arr[i] << "\n";
-                // cout << "Array now: \t";
-                // for (int n : arr) {
-                //     cout << n << ", ";
-                // }
-                // cout << "\n";
-            }
+    // Traverse arr[;ow..high] and move all smaller
+    // elements on left side. Elements from low to 
+    // i are smaller after every iteration
+    for (int j = low; j <= high - 1; j++) {
+        if (arr[j] < pivot) {
+            i++;
+            swap(arr[i], arr[j]);
         }
-
-        // Set pivot in position
-        int pivot_index = before_index+1;
-        swap(arr[pivot_index], arr[end]);
-
-        // Continue recursively on the two halves
-        quickSort(arr, start, pivot_index - 1, layer+1);
-        quickSort(arr, pivot_index + 1, end, layer+1);
-
-        cout << "Array at the layer " << layer << ":\t";
-        for (int n : arr) {
-            cout << n << ", ";
-        }
-        cout << "\n";
     }
-    return arr;
+    
+    // Move pivot after smaller elements and
+    // return its position
+    swap(arr[i + 1], arr[high]);  
+    return i + 1;
+}
+
+// The QuickSort function implementation
+void quickSort(vector<int>& arr, int low, int high) {
+  
+    if (low < high) {
+      
+        // pi is the partition return index of pivot
+        int pi = partition(arr, low, high);
+
+        // Recursion calls for smaller elements
+        // and greater or equals elements
+        quickSort(arr, low, pi - 1);
+        quickSort(arr, pi + 1, high);
+    }
+}
+
+void quickSort1(vector<int>& vec, int start, int end) {
+    if (start >= end) {
+        // There are less than 2 elements
+        return;
+    }
+
+    int size = vec.size();
+
+    int pivot = vec[end];
+    for (int i=0; i<size; i++) {
+        if (vec[i] > pivot) {
+
+        }
+    }
 }
 
 int main() {
     vector<int> numbers = {915, 960, 152, 514, 1325, 900, 1452, 637, 1085, 1282};
-    quickSort(numbers, 0, numbers.size()-1, 0);
+    quickSort(numbers, 0, numbers.size()-1);
     for (int n : numbers) {
         cout << n << " ";
     }
