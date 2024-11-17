42948d7549eddcd720f3e8fb9eb293aa1f690cc9
oskarziembrowicz <rakson2002@gmail.com>
2024-11-17 13:47:45 +0100

    Created a nobles tailored bucket sort

--- lab5/algorithm-2-c.cpp
+++ lab5/algorithm-2-c.cpp
@@ -90,43 +90,6 @@ void quickSort(vector<int>& vec, int start, int end) {
     quickSort(vec, swapIter+1, end);
 }
 
-void insertionSort(vector<float>& bucket) {
-    for (int i = 1; i < bucket.size(); ++i) {
-        float key = bucket[i];
-        int j = i - 1;
-        while (j >= 0 && bucket[j] > key) {
-            bucket[j + 1] = bucket[j];
-            j--;
-        }
-        bucket[j + 1] = key;
-    }
-}
-
-void bucketSort(vector<float>& arr, int n) {
-    const int n = arr.size();
-    // 1) Create n empty buckets
-    vector<vector<float>> b(n);
-
-    // 2) Put array elements in different buckets
-    for (int i = 0; i < n; i++) {
-        int bi = n * arr[i];
-        b[bi].push_back(arr[i]);
-    }
-
-    // 3) Sort individual buckets using insertion sort
-    for (int i = 0; i < n; i++) {
-        insertionSort(b[i]);
-    }
-
-    // 4) Concatenate all buckets into arr[]
-    int index = 0;
-    for (int i = 0; i < n; i++) {
-        for (int j = 0; j < b[i].size(); j++) {
-            arr[index++] = b[i][j];
-        }
-    }
-}
-
 int main() {
     // n - liczba szlachciców
     // s - inicjał szlachcica
--- lab5/bucket-sort-test.cpp
+++ lab5/bucket-sort-test.cpp
@@ -3,86 +3,107 @@
 
 using namespace std;
 
-// A - 65
-// Z - 90
-const int N_OF_CHARS = 90-65;
+const int N_OF_LETTERS = int('Z')-int('A')+1;
+
+struct Noble {
+    char initial;
+    Noble(char initial) {
+        this->initial = initial;
+    }
+};
 
 int charToAsciiArr(char c) {
     return int(c) - 65;
 }
 
-void insertionSort(vector<int>& bucket) {
-    for (int i = 1; i < bucket.size(); ++i) {
-        float key = bucket[i];
-        int j = i - 1;
-        while (j >= 0 && bucket[j] > key) {
-            bucket[j + 1] = bucket[j];
-            j--;
-        }
-        bucket[j + 1] = key;
+// void insertionSort(vector<int>& bucket) {
+//     for (int i = 1; i < bucket.size(); ++i) {
+//         float key = bucket[i];
+//         int j = i - 1;
+//         while (j >= 0 && bucket[j] > key) {
+//             bucket[j + 1] = bucket[j];
+//             j--;
+//         }
+//         bucket[j + 1] = key;
+//     }
+// }
+
+// void bucketSort(vector<char>& arr) {
+//     // const int n = arr.size();
+//     // 1) Create n empty buckets
+//     vector<vector<char>> b(N_OF_LETTERS);
+
+//     // BUCKETS ARE OUT OF ORDER
+
+//     // 2) Put array elements in different buckets
+//     for (int i = 0; i < arr.size(); i++) {
+//         // int tmp = turnToSmall(arr[i]) * n;
+//         // b[tmp].push_back(arr[i]);
+
+//         int tmp = charToAsciiArr(arr[i]);
+//         b[tmp].push_back(arr[i]);
+
+//         // int bi = n * turnToSmall([i]);
+//         // b[bi].push_back(arr[i]);
+//     }
+
+//     // THIS IS OK:
+//     for (int i=0; i<N_OF_LETTERS; i++) {
+//         cout << i << ": ";
+//         for (int j = 0; j < b[i].size(); j++) {
+//             cout << b[i][j] << " ";
+//         }
+//         cout << "\n";
+//     }
+//     cout << "\n";
+
+//     // 3) Sort individual buckets using insertion sort
+//     for (int i = 0; i < n; i++) {
+//         insertionSort(b[i]);
+//     }
+
+//     // 4) Concatenate all buckets into arr[]
+//     int index = 0;
+//     for (int i = 0; i < N_OF_LETTERS; i++) {
+//         for (int j = 0; j < b[i].size(); j++) {
+//             arr[index++] = b[i][j];
+//             cout << arr[index-1] << "\n";
+//         }
+//     }
+//     cout << arr.size() << "\n";
+//     cout << "checkpoint 1\n";
+//     return;
+// }
+
+void bucketSortNobles(vector<Noble>& vec) {
+    // Create empty buckets
+    vector<vector<Noble>> buckets(N_OF_LETTERS);
+
+    // Put vector elements in different buckets based on ascii
+    for (int i = 0; i < vec.size(); i++) {
+        int charIndex = charToAsciiArr(vec[i].initial);
+        buckets[charIndex].push_back(vec[i]);
     }
-}
 
-void bucketSortNobles(vector<char>& arr) {
-    // const int n = arr.size();
-    // 1) Create n empty buckets
-    vector<vector<char>> b(N_OF_CHARS);
-
-    // BUCKETS ARE OUT OF ORDER
-
-    // 2) Put array elements in different buckets
-    for (int i = 0; i < arr.size(); i++) {
-        // int tmp = turnToSmall(arr[i]) * n;
-        // b[tmp].push_back(arr[i]);
-
-        int tmp = charToAsciiArr(arr[i]);
-        b[tmp].push_back(arr[i]);
-
-        // int bi = n * turnToSmall([i]);
-        // b[bi].push_back(arr[i]);
-    }
-
-    // THIS IS OK:
-    // for (int i=0; i<N_OF_CHARS; i++) {
-    //     cout << i << ": ";
-    //     for (int j = 0; j < b[i].size(); j++) {
-    //         cout << b[i][j] << " ";
-    //     }
-    //     cout << "\n";
-    // }
-    // cout << "\n";
-
-    // 3) Sort individual buckets using insertion sort
-    // for (int i = 0; i < n; i++) {
-    //     insertionSort(b[i]);
-    // }
-
-    // 4) Concatenate all buckets into arr[]
+    // Concatenate all buckets into vec
     int index = 0;
-    for (int i = 0; i < N_OF_CHARS; i++) {
-        for (int j = 0; j < b[i].size(); j++) {
-            arr[index++] = b[i][j];
-            cout << arr[index-1] << "\n";
+    for (int i = 0; i < N_OF_LETTERS; i++) {
+        for (int j = 0; j < buckets[i].size(); j++) {
+            vec[index++] = buckets[i][j];
         }
     }
-    cout << arr.size() << "\n";
-    cout << "checkpoint 1\n";
     return;
 }
 
 int main() {
     // vector<int> numbers = {915, 960, 152, 514, 1325, 900, 1452, 637, 1085, 1282};
     vector<char> initials = {'X', 'Z', 'X', 'F', 'E'};
+    vector<Noble> nobles = {Noble('X'), Noble('Z'), Noble('X'), Noble('F'), Noble('E')};
 
-    bucketSortNobles(initials);
+    bucketSortNobles(nobles);
 
-    // CODE STOPS HERE WTF?!
-    
-    cout << "checkpoint 2\n";
-    cout << initials.size();
-
-    for (char e : initials) {
-        cout << e << " ";
+    for (Noble n : nobles) {
+        cout << n.initial << " ";
     }
     cout << "\n";
 
