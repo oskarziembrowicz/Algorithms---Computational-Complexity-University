b8154a06bc76ba2db83e4a4fe2f41a6ef908f4a4
oskarziembrowicz <rakson2002@gmail.com>
2024-11-22 14:10:32 +0100

    Changed int to long long - algorithm finished

--- lab6/algorithm-3-b.cpp
+++ lab6/algorithm-3-b.cpp
@@ -2,11 +2,7 @@
 
 using namespace std;
 
-// GOOD TIME !!
-// SOMETIMES WRONG ANSWER
-// MEMORY BREACH
-
-void printField(int** field, int height, int width) {
+void printField(long long** field, int height, int width) {
     for (int i=0; i<height; i++) {
         for (int j=0; j<width; j++) {
             cout << field[i][j] << " ";
@@ -16,7 +12,7 @@ void printField(int** field, int height, int width) {
     cout << "\n";
 }
 
-void addToField(int** arr, int rows, int cols, int x1, int y1, int x2, int y2, int amount) {
+void addToField(long long** arr, int rows, int cols, int x1, int y1, int x2, int y2, long long amount) {
     arr[y1][x1] += amount;
     if (y2 < rows-1) {
         arr[y2+1][x1] -= amount;
@@ -29,7 +25,7 @@ void addToField(int** arr, int rows, int cols, int x1, int y1, int x2, int y2, i
     }
 }
 
-void prefixSum(int** arr, int rows, int cols) {
+void prefixSum(long long** arr, int rows, int cols) {
     // Sum first column
     for (int i=1; i<rows; i++) {
         arr[i][0] += arr[i-1][0];
@@ -47,8 +43,8 @@ void prefixSum(int** arr, int rows, int cols) {
     }
 }
 
-int areaSum(int** prefixArray, int x1, int y1, int x2, int y2) {
-    int prevInCol = 0, prevInRow = 0, prev = 0;
+long long areaSum(long long** prefixArray, int x1, int y1, int x2, int y2) {
+    long long prevInCol = 0, prevInRow = 0, prev = 0;
     if (x1 > 0 && y1 > 0) {
         prev = prefixArray[y1-1][x1-1];
     }
@@ -68,20 +64,21 @@ int main() {
     int fieldCols, fieldRows;
     cin >> numberOfAnalisys >> fieldCols >> fieldRows;
 
-    int** field = new int*[fieldRows];
+    long long** field = new long long*[fieldRows];
 
     for (int i=0; i<numberOfAnalisys; i++) {
         // Initialize array with zeros
         for (int j=0; j<fieldRows; j++) {
-            field[j] = new int[fieldCols]();
+            field[j] = new long long[fieldCols]();
         }
         // printField(field, fieldRows, fieldCols);
 
         // Get amount of updates to field
-        int dataAmount;
-        cin >> dataAmount;
-        for (int j=0; j<dataAmount; j++) {
-            int x1, x2, y1, y2, fertilizerAmount;
+        int updatesAmount;
+        cin >> updatesAmount;
+        for (int j=0; j<updatesAmount; j++) {
+            int x1, x2, y1, y2;
+            long long fertilizerAmount;
             cin >> x1 >> y1 >> x2 >> y2 >> fertilizerAmount;
             
             addToField(field, fieldRows, fieldCols, x1, y1, x2, y2, fertilizerAmount);
@@ -97,7 +94,7 @@ int main() {
         int numberOfQuerys;
         cin >> numberOfQuerys;
 
-        int* output = new int[numberOfQuerys];
+        long long* output = new long long[numberOfQuerys];
 
         for (int j=0; j<numberOfQuerys; j++) {
             // Query
@@ -105,7 +102,6 @@ int main() {
             cin >> x1 >> y1 >> x2 >> y2;
 
             output[j] = areaSum(field, x1, y1, x2, y2);
-            // cout << areaSum(field, x1, y1, x2, y2) << " " ;
         }
 
         // Show output for this analisys
