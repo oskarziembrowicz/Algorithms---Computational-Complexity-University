e4e07683fcce88f9937a9080d44445f876278405
oskarziembrowicz <rakson2002@gmail.com>
2024-11-23 17:31:14 +0100

    Wrote count sort from memory

--- revision1/count-sort.cpp
+++ revision1/count-sort.cpp
@@ -35,13 +35,41 @@ void countSortUnstable(int* arr, int size) {
     }
 }
 
+void prefixSum(int* arr, int size) {
+    for (int i=1; i<size; i++) {
+        arr[i] += arr[i-1];
+    }
+}
+
+int* countSort(int* arr, int size) {
+    int max = maximum(arr, size);
+
+    int* countArr = new int[max+1]();
+    for (int i=0; i<size; i++) {
+        countArr[arr[i]]++;
+    }
+
+    prefixSum(countArr, max+1);
+
+    int* outputArr = new int[size];
+
+    for (int i=size-1; i>=0; i--) {
+        outputArr[--countArr[arr[i]]] = arr[i];
+    }
+
+    delete[] countArr;
+    return outputArr;
+}
+
 int main() {
     int size = 10;
     int* arr = new int[size] {20, 1, 45, 432, 78, 900, 89435, 22, 45, 11};
 
-    countSortUnstable(arr, size);
+    arr = countSort(arr, size);
 
     printArray(arr, size);
+
+    delete[] arr;
 
     return 0;
 }
